34edc4056bafe03562cd48f966fdfa9cbe2f9ad2
nzbenji <benlittle53@gmail.com>
2020-03-29 10:11:59 +1300

    added followers and following models

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +37,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function following() {
+        return $this->belongsToMany(
+            User::class, 'followers', 'user_id', 'following_id'
+        );
+        
+    }
+
+    public function followers() {
+        return $this->belongsToMany(
+            User::class, 'followers', 'following_id', 'user_id'
+        );
+        
+    }
 }
