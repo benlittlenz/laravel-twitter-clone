4694e3d437def9374790f7d0f893f0c489058b9e
nzbenji <benlittle53@gmail.com>
2020-03-29 09:53:26 +1300

    setup airlock

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>{{user}}</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default {
+
+        data() {
+            return {
+                user: null
+            }
+        },
+        methods: {
+            async getUser() {
+                let res = await axios.get('/api/user')
+
+                this.user = res.data;
+            }
+        },
+
+        mounted() {
+            this.getUser();
+            console.log(user)
+        }
+    }
+</script>
