d624b3eb1e8871c2f0b803bb0f44ab802f527431
nzbenji <benlittle53@gmail.com>
2020-04-07 19:56:07 +1200

    setup websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,7 +36,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: true
+    key: 'local',
+    encrypted: false,
+    disableStats: true,
+    wsHost: window.location.hostname,
+    wsPort: 6001
 });
